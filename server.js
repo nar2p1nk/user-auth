0d27db309216722909e8bbd16e507034145806d6
nar2p1nk <farrisisqandar@gmail.com>
2022-02-01 01:15:45 +0800

    tried sending POST to login,i failed,trying again tomorrow

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const ejs = require('ejs')
 const app = express()
+const passport = require('passport')
 const database = require('./database');
+const init = require('./userAuth')
+const session = require('express-session');
+
 app.set('view-engine','ejs')
+init(passport)
 
 app.use(express.urlencoded({extended:false}))
+app.use(session({
+    secret:'CHEESEBURGER UGHHH',
+    resave:false,
+    saveUninitialized:false
+}))
+
+app.use(passport.initialize())
+
+app.use(passport.session())
 
 app.get('/register',(req,res)=>{
     res.render('register.ejs')
@@ -19,6 +33,28 @@ app.post('/register',(req,res)=>{
 app.get('/login',(req,res)=>{
     res.render('login.ejs')
 })
+
+app.post('/login',checkNotAuth,passport.authenticate('local',{
+    successRedirect:'/',
+    failureRedirect:'/login',
+    failureFlash:true
+}))  
+
+function checkAuth(req,res,next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect('/login')
+}
+
+function checkNotAuth(req,res,next){
+    if(req.isAuthenticated()){
+        return res.redirect('/')
+    }
+    return next()
+}
+
+
 
 app.listen(8080,()=>{
     console.log(`server listening at localhost:8080`)
--- userAuth.js
+++ userAuth.js
@@ -1,17 +1,31 @@
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
+const sqlite = require('sqlite3')
 
-function init(password,getUserByName){
+const db = new sqlite.Database('users.db')
+
+function init(passport,name){
     const auth = async(name,password,done)=>{
-        const user = getUserByName(name)
-        if(user == null){
+        db.get(`SELECT * FROM user WHERE name = ?`,[name],async (err,row) =>{
+
+        if(row == null){
             return done(null,false,{message:'no user found'})
         }
         try{
-            if (await bcrypt.compare(password,user.password)){
-                return done(null,user)
+            if(await bcrypt.compare(password,row.password)){
+                return done(null,row)
             }
         }
         catch(e){return done(e)}
-    }
+    } )}
+    passport.use(new LocalStrategy({usernameField:'name'},auth))
+    passport.serializeUser((row,done)=> done(null,row.Id) )
+    passport.deserializeUser((id,done)=> done(null,getUserById(id))
+    )
+
 }
+
+module.exports = init
+
+
+
